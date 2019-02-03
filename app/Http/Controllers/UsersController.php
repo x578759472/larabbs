722ac207648ab495fc8fe670b23ebd4e6728acca
许力 <578759472@qq.com>
2019-02-03 10:02:19 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,12 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
+
     public function show(User $user)
     {
         return view('users.show',compact('user'));
@@ -18,6 +24,7 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
@@ -25,7 +32,7 @@ class UsersController extends Controller
 
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
-
+        $this->authorize('update',$user);
         $data = $request->all();
         if($request->avatar){
             $result = $uploader->save($request->avatar,'avatars',$user->id,416);
