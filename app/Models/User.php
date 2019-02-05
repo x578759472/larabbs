7eaae90441aca93af6016169ec32f054a04da2e7
许力 <578759472@qq.com>
2019-02-05 06:25:50 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,13 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
+
+
 }
