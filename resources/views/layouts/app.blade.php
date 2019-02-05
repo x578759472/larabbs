11924d6692e43ff81e537eccb433c63b64f57285
许力 <578759472@qq.com>
2019-02-05 12:52:41 +0000

    站点设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="csrf-token" content="{{csrf_token()}}">
 
-  <title>@yield('title','laraBBS') - Laravel 进阶教程</title>
-  <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
+  <title>@yield('title','laraBBS') - {{setting('site_name','Laravel 进阶教程')}}</title>
+  <meta name="description" content="@yield('description', setting('seo_description','LaraBBS 爱好者社区。'))" />
+  <meta name="keyword" content="@yield('keyword', setting('seo_keyword','LaraBBS,社区,论坛,开发者论坛'))" />
 
 <link rel="stylesheet" href="{{mix('css/app.css')}}">
 
