7400e3e8bc187b62627cb300f812915ac4cc8cdd
许力 <578759472@qq.com>
2019-02-08 06:29:21 +0000

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,19 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category,Topic $topic,Request $request,User $user)
+    public function show(Category $category,Topic $topic,Request $request,User $user,Link $link)
     {
         $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
 
         $active_users = $user->getActiveUsers();
 
-
-        return view('topics.index',compact('topics','category','active_users'));
+        $links = $link->getAllCached();
+        return view('topics.index',compact('topics','category','active_users','links'));
     }
 }
